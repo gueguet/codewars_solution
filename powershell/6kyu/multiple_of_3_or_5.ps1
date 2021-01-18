4d20f0840487237ed98a3582c86496ec0f640874
Gaétan Verdin-Pol <gaetan.verdinpol@gmail.com>
2021-01-19 00:28:00 +0100

    pwsh - 6kyu : multiple of 3 and 5

---
+++ powershell/6kyu/multiple_of_3_or_5.ps1
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/514b92a657cdc65150000006/train/powershell
+
+function Get-SumOfMultiples($Value)
+{
+  $sum = 0
+
+  for($i = 0; $i -lt $Value; $i++)
+  {
+    if ($i%3 -eq 0 -or $i%5 -eq 0) {
+      $sum = $sum + $i
+    }
+  }
+  $sum
+
+}
+
+Get-SumOfMultiples 10
