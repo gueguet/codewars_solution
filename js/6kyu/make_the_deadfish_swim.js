4417fd95c20c37059e75d9dcc4e0e32b4b29f8cd
Gaétan Verdin-Pol <gaetan.verdinpol@gmail.com>
2021-01-19 01:09:15 +0100

    js - 6kyu : make the deadfish swim

---
+++ js/6kyu/make_the_deadfish_swim.js
@@ -0,0 +1,33 @@
+// https://www.codewars.com/kata/51e0007c1f9378fa810002a9/train/javascript
+
+function parse( data )
+{
+  
+  let res = new Array();
+  let val = 0;
+
+  Array.from(data).forEach((char) => {
+  
+    if (char === "o") {
+      res.push(val);
+    }
+
+    if (char === "i") {
+      val += 1;
+    }
+
+    if (char === "d") {
+      val -= 1;
+    }
+
+    if (char === "s") {
+      val = val * val;
+    }
+  
+  });
+
+  return res;
+
+}
+
+parse("iiisdoso")
