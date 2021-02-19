e65d9cb63501d758875018327d57d69bcb48159d
gueguet <gaetan.verdinpol@gmail.com>
2021-02-19 19:40:44 +0100

    python - 8kyu : sum arrays

---
+++ python/8kyu/sum_arrays.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/python
+
+def sum_array(a):
+    if (a == []):
+        return 0
+    return sum(a)
+
+print(sum_array([1.1, 2.2, 3.3]))
