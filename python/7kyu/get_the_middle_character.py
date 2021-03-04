136f26e5eda9c6dd1421462ecea8d3ea8ac7a893
gueguet57 <gaetan.verdinpol@gmail.com>
2021-03-04 14:32:02 +0100

    python - 7kyu : get the middle character

---
+++ python/7kyu/get_the_middle_character.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/56747fd5cb988479af000028/train/python
+
+def get_middle(s):
+    if (len(s) % 2 == 0):
+        return (s[(len(s)//2)-1:(len(s)//2)+1])
+    else:
+        return (s[len(s)//2])
+        
+get_middle("test")
