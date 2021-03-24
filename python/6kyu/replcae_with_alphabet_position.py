e6bb6792f78032419a0530fa7fe82b2ab79b6e15
Gaétan Verdin-Pol <gaetan.verdinpol@gmail.com>
2021-03-24 15:13:31 +0100

    python - 6 kyu : replace with alphabet position

---
+++ python/6kyu/replcae_with_alphabet_position.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/546f922b54af40e1e90001da/train/python
+
+from string import ascii_lowercase
+
+position_list = [str(i) for i in range (1,27)]
+
+def alphabet_position(text):
+    text = text.lower()
+    alpha_dict = dict(zip(ascii_lowercase, position_list))
+    res = ""
+    for char in text:
+        if (char in alpha_dict):
+            res += alpha_dict[char] + " "
+    return res[:-1]
+
+
+alphabet_position("The sunset sets at twelve o' clock.")
