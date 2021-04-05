84027bc81ff15ec6739fd89667be3a6af5818341
gueguet57 <gaetan.verdinpol@gmail.com>
2021-04-05 10:37:54 +0200

    merge the repo with my old codewars solutions

---
+++ python/5kyu/members_since.py
@@ -0,0 +1,61 @@
+# https://www.codewars.com/kata/58ab2ed1acbab2eacc00010e/train/python
+
+# webpage : https://www.codewars.com/users/jhoffner
+
+
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import ssl 
+import unittest
+
+ssl._create_default_https_context = ssl._create_unverified_context # ignore certificates ssl 
+
+def get_member_since(username):
+
+    response = urllib.request.urlopen('https://www.codewars.com/users/{}'.format(username))
+    html = response.read()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    div_to_regex = ""
+    stat_divs = soup.findAll("div", {"class": "stat"})
+    for div in stat_divs:
+        if "Member Since" in str(div):
+            div_to_regex = str(div)
+
+    pattern = "(\/b>)(.+?)(?=</div)"
+
+    # res_username = re.match(pattern, username_info).group(2)
+    res_date = re.findall(pattern, div_to_regex)[0][1]
+
+    return res_date
+
+    
+
+# test 
+
+class Test(unittest.TestCase):
+
+    def test_member_since(self):
+
+        self.assertEqual(get_member_since('jhoffner'), 'Oct 2012')
+        self.assertEqual(get_member_since('ecolban'), 'Mai 2015')
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
+
+
+# cleaner / shorter solution
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+def get_member_since(username):
+    html = urlopen(f'https://www.codewars.com/users/{username}')
+    soup = bs(html.read(), "html.parser")
+    tags = soup.find_all("div", {"class": "stat"})
+    member_tag = [x.text for x in tags if 'Member Since' in x.text][0]
+    return member_tag.split(':')[1]
