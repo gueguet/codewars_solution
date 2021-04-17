da54f55bd870495034dd79a30824b4c27dcacb4d
gueguet57 <gaetan.verdinpol@gmail.com>
2021-04-17 12:30:59 +0200

    bash - 8kyu : returning Strings

---
+++ shell/8kyu/returning_string.sh
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/55a70521798b14d4750000a4/train/shell
+
+#!/bin/bash
+
+# if we wanted a user input :
+# read -p "Name : " name
+
+name=$1
+echo "Hello, $name how are you doing today?"
+
+
+# one line
+# echo "Hello, $1 how are you doing today?"
