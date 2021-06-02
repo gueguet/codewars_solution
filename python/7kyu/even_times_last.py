7635c0fe16cd444bbb935fd29ae7b67be191e229
Gaetan Verdin-Pol <gueguet57@Gaetans-MacBook-Pro.local>
2021-06-02 23:27:21 +0200

    python - 6kyu : even times last

---
+++ python/7kyu/even_times_last.py
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/5a1a9e5032b8b98477000004/train/python
+
+def even_last(numbers): 
+    
+    if numbers == []:
+        return 0
+
+    res = 0
+    
+    for i in range(0,len(numbers),2):
+        res += numbers[i]
+    
+    res = res * numbers[-1]
+    return res
+
+even_last([2, 3, 4, 5])
+
+
+
+"""
+* clever
+
+def even_last(numbers): 
+    return sum(numbers[::2]) * numbers[-1] if numbers else 0
+
+"""
