3ab3f7d7f77b82b622384a0472a041e554a70959
gueguet <gaetan.verdinpol@gmail.com>
2021-06-23 23:39:32 +0200

    python - 6kyu : catalog.py

--- python/6kyu/catalog.py
+++ python/6kyu/catalog.py
@@ -1,4 +1,79 @@
 # https://www.codewars.com/kata/59d9d8cb27ee005972000045/train/python
 
+import re
+
 def catalog(s, article):
-    # your code
+
+  lines = s.split('\n')
+  res = []
+
+  for line in lines:
+    if (article in line):
+      name = re.findall("<name>(.*?)<\/name>", line)[0]
+      price = re.findall("<prx>(.*?)<\/prx>", line)[0]
+      quantity = re.findall("<qty>(.*?)<\/qty>", line)[0]
+      res.append('{} > prx: ${} qty: {}'.format(name, price, quantity))
+  
+  if (len(res) == 0):
+    return 'Nothing'
+
+  if (len(res) == 1):
+    return ''.join(res)
+
+  else:
+    return '\r\n'.join(res)
+
+
+
+
+
+s = """<prod><name>drill</name><prx>99</prx><qty>5</qty></prod>
+
+<prod><name>hammer</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>screwdriver</name><prx>5</prx><qty>51</qty></prod>
+
+<prod><name>table saw</name><prx>1099.99</prx><qty>5</qty></prod>
+
+<prod><name>saw</name><prx>9</prx><qty>10</qty></prod>
+
+<prod><name>chair</name><prx>100</prx><qty>20</qty></prod>
+
+<prod><name>fan</name><prx>50</prx><qty>8</qty></prod>
+
+<prod><name>wire</name><prx>10.8</prx><qty>15</qty></prod>
+
+<prod><name>battery</name><prx>150</prx><qty>12</qty></prod>
+
+<prod><name>pallet</name><prx>10</prx><qty>50</qty></prod>
+
+<prod><name>wheel</name><prx>8.80</prx><qty>32</qty></prod>
+
+<prod><name>extractor</name><prx>105</prx><qty>17</qty></prod>
+
+<prod><name>bumper</name><prx>150</prx><qty>3</qty></prod>
+
+<prod><name>ladder</name><prx>112</prx><qty>12</qty></prod>
+
+<prod><name>hoist</name><prx>13.80</prx><qty>32</qty></prod>
+
+<prod><name>platform</name><prx>65</prx><qty>21</qty></prod>
+
+<prod><name>car wheel</name><prx>505</prx><qty>7</qty></prod>
+
+<prod><name>bicycle wheel</name><prx>150</prx><qty>11</qty></prod>
+
+<prod><name>big hammer</name><prx>18</prx><qty>12</qty></prod>
+
+<prod><name>saw for metal</name><prx>13.80</prx><qty>32</qty></prod>
+
+<prod><name>wood pallet</name><prx>65</prx><qty>21</qty></prod>
+
+<prod><name>circular fan</name><prx>80</prx><qty>8</qty></prod>
+
+<prod><name>exhaust fan</name><prx>62</prx><qty>8</qty></prod>
+
+<prod><name>window fan</name><prx>62</prx><qty>8</qty></prod>"""
+
+
+catalog(s, 'saw')
