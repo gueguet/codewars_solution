87b70966a689385dec6cc5369a19e43ca115781b
gueguet <gaetan.verdinpol@gmail.com>
2022-02-02 20:01:10 +0100

    python - 5kyu : regex password validation

--- python/5kyu/regex_password_validation.py
+++ python/5kyu/regex_password_validation.py
@@ -6,3 +6,20 @@ regex = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$"
 
 # print(bool(re.search(regex, 'fjd3IR9')))
 print(bool(re.search(regex, '7i!qogiPKkkS8Q')))
+
+
+"""
+* clever 
+
+from re import compile, VERBOSE
+
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)    
+"""
