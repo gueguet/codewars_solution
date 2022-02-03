bbea0d6d9a74aae1fe81d68af1a2cc5c9c8c74ef
gueguet <gaetan.verdinpol@gmail.com>
2022-02-03 21:53:43 +0100

    python - 7kyu : digitize

---
+++ python/7kyu/digitize.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5417423f9e2e6c2f040002ae/train/python
+
+def digitize(n):
+    return [int(digit) for digit in str(n)]
+        
+"""
+* clever
+
+def digitize(n):
+    return list(map(int, str(n)))
+"""
