dd2619ca1af522f22954ee8525831122b33ef190
gueguet <gaetan.verdinpol@gmail.com>
2022-03-09 20:15:31 +0100

    python - 6kyu : assemble string

--- python/6kyu/assemble_string.py
+++ python/6kyu/assemble_string.py
@@ -18,4 +18,20 @@ def assemble(arr):
 print(assemble(['H*llo, W*rld!', 'Hel*o, *or*d!', '*ello* World*']))
 
 
+"""
+* clever
 
+def assemble(arr):
+    return ''.join(c.discard('*') or c and c.pop() or '#' for c in map(set, zip(*arr)))
+    
+--------------------------------------------------------------------------------------
+    
+def assemble(input):
+    result = list(input[0]) if input else []
+    
+    for i in input:
+        for j, k in enumerate(i):
+            result[j] = k if result[j] == '*' else result[j]
+    
+    return ''.join(result).replace('*', '#')
+"""
